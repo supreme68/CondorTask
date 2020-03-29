460b170b0f3437160c2e974a11b29969064ad751
Dimitar Arnaudov <dimitararnaudov68@gmail.com>
2020-03-29 04:16:53 +0300

    optimsed structure

--- gulpfile.js
+++ gulpfile.js
@@ -1,74 +1,42 @@
-const scssInput = ['scss/style.scss'],
-    scssOutput = 'app/css',
-    vendorInput = [
-        'scripts/vendor/*.js'
-    ],
-    jsInput = [
-        'scripts/domain/*.js'
-    ],
-    jsOutput = 'app/scripts';
-
-// Start everything up.
-const gulp = require('gulp');
+const {series, src, dest} = require("gulp");
 const sass = require('gulp-sass');
-const autoprefixer = require('gulp-autoprefixer');
+const browserSync = require('browser-sync').create();
 const sourcemaps = require('gulp-sourcemaps');
+const autoprefixer = require('gulp-autoprefixer');
+const uglify = require('gulp-uglify');
+const rename = require('gulp-rename');
 const babel = require('gulp-babel');
 const concat = require('gulp-concat');
-const rename = require('gulp-rename');
-const uglify = require('gulp-uglify');
-const browserSync = require('browser-sync').create();
-const plumber = require('gulp-plumber');
+const fs   = require('fs');
 
 
-// Watch SASS.
-gulp.task('sass', function() {
-    return gulp
-        .src(scssInput)
-        .pipe(plumber())
+const jsInput = 'app/scripts/*.js';
+
+
+function sassBuild(){
+        return src('scss/style.scss')
+            .pipe(sourcemaps.init())
+            .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+            .pipe(autoprefixer())
+            .pipe(sourcemaps.write())
+            .pipe(dest("bin/"))
+            .pipe(browserSync.reload({
+                stream:true
+            }))
+}
+
+function minifyJS(){
+    return src('app/scripts/scripts.js')
         .pipe(sourcemaps.init())
-        .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
-        .pipe(autoprefixer())
-        .pipe(sourcemaps.write())
-        .pipe(gulp.dest(scssOutput))
-        .pipe(browserSync.reload({
-            stream: true
+        ,pipe(babe({
+            presets:["env"]
         }))
-});
-
-gulp.task('domainScripts', function() {
-    return gulp.src(jsInput)
-        .pipe(plumber())
-        .pipe(sourcemaps.init())
-        .pipe(babel({
-            presets: ['env']
-        }))
-        .pipe(concat('scripts.js'))
-        .pipe(gulp.dest(jsOutput))
-        .pipe(rename('scripts.min.js'))
+        .pipe(concat("scripts.js"))
+        .pipe(dest('app/scripts'))
+        .pipe(rename("scripts.min.js"))
         .pipe(uglify())
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest(jsOutput));
-});
+        .pipe(sourcemaps.write("."))
+        .pipe(dest())
+}
 
-gulp.task('vendorScripts', function() {
-    return gulp.src(vendorInput)
-        .pipe(plumber())
-        .pipe(concat('vendor.js'))
-        .pipe(gulp.dest(jsOutput));
-});
-
-gulp.task('browserSync', function() {
-    browserSync.init({
-        server: {
-            baseDir: 'app'
-        },
-    })
-})
-
-gulp.task('watch', ['sass', 'domainScripts', 'vendorScripts', 'browserSync'], function (){
-    gulp.watch('scss/**/*.scss', ['sass', browserSync.reload]);
-    gulp.watch('app/**/*.html', browserSync.reload);
-    gulp.watch('scripts/domain/**/*.js', ['domainScripts', browserSync.reload]);
-    gulp.watch('scripts/vendor/**/*.js', ['vendorScripts', browserSync.reload]);
-});
+exports.build = series(sassBuild);
